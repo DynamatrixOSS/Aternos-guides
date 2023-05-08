7d91a8923ddce0d4bd1583ae7d9dcfadc5d9a68b
Dobby-progam <87517487+Dobby-progam@users.noreply.github.com>
2023-05-08 17:23:24 +0200

    nothing

--- index.php
+++ index.php
@@ -25,15 +25,15 @@ require_once('src/models/functions.php')
                     <span>We are not an official site, and we are not affiliated with Aternos, Aternos GmbH, exaroton and it's associates and employees.</span>
                 </div>
             </div>
-            <p>Welcome to Aternos Guides, an article site ran by Aternos regulars and an Aternos moderator. Here you may find all kinds of articles about topics and issues that are not covered in Aternos' help center itself. Have an issue that you don't know the fix to? Try our articles below, in the articles page or join the Aternos discord for help.</p>
+            <p><b>Welcome to Aternos Guides</b><br> An article site ran by Aternos regulars and an Aternos moderator. Here you may find all kinds of articles about topics and issues that are not covered in Aternos' help center itself. Have an issue that you don't know the fix to? Try our articles below, in the articles page or join the Aternos discord for help.</p>
 
-            <a href="about.php"><button class="btn btn-primary">About Us</button></a>
+            <a href="About.php"><button class="btn btn-primary">About Us</button></a>
             <a href="articles.php"><button class="btn btn-outline-primary">Articles</button></a>
             <hr>
         </div>
 
         <div class="container pt-5">
-            <div class="float-start" style="margin-left: 25%">
+            <div class="float-start o25p">
                 <h2>Popular articles</h2>
                 <ul class="hidden">
                     <li>Example article 1</li>
@@ -42,7 +42,7 @@ require_once('src/models/functions.php')
                 </ul>
             </div>
 
-            <div class="float-end" style="margin-right: 25%">
+            <div class="float-end o25p">
                 <h2>Pinned articles</h2>
                 <ul class="hidden">
                     <li>Example article 1</li>
