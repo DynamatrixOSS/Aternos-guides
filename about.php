73ad48578fdd10af9bac9c88732957938d03ed63
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-08 22:48:11 +0200

    Debug properly....

--- about.php
+++ about.php
@@ -10,7 +10,7 @@ require_once('src/models/functions.php')
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <?php $pageName = getPageName(basename($_SERVER['PHP_SELF']));?>
         <title><?php echo $pageName ?></title>
-        <?php echo "<script>console.log(' .$pageName')</script>" ?>
+        <?php     echo "<script>console.log('debug object: " . $pageName . "');</script>"; ?>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     </head>
 
