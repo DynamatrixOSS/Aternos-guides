a016a6e963afd24e086938adcef46385f24df259
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-08 15:51:49 +0200

    Add fontawesome

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ require_once('src/models/functions.php')
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?php  echo getPageName(basename($_SERVER['PHP_SELF'])) ?></title>
+        <script src="https://kit.fontawesome.com/d1393c407a.js" crossorigin="anonymous"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     </head>
 
