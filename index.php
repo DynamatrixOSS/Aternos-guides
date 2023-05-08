f51341ec7efab7734da7e8cb3886ee4658d304b1
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-08 15:52:01 +0200

    Add main.css link

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ require_once('src/models/functions.php')
         <title><?php  echo getPageName(basename($_SERVER['PHP_SELF'])) ?></title>
         <script src="https://kit.fontawesome.com/d1393c407a.js" crossorigin="anonymous"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+        <link rel="stylesheet" href="src/styling/main.css">
     </head>
 
     <?php include_once 'src/models/navbar.php' ?>
