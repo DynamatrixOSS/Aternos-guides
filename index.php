64eb8ade2771d0fa21b2a3729ac8360931a6beaf
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-08 15:52:11 +0200

    Add body

--- index.php
+++ index.php
@@ -17,6 +17,41 @@ require_once('src/models/functions.php')
     <?php include_once 'src/models/navbar.php' ?>
 
     <body>
+        <div class="text-center pt-5 container">
+            <h1>Aterguides</h1>
+            <div class="card border-warning bg-warning-subtle my-5">
+                <div class="card-body">
+                    <i class="fa-solid fa-triangle-exclamation fa-fade fa-2xl" style="color: #ffa50a;"></i>
+                    <span>We are not an official site, and we are not affiliated with Aternos, Aternos GmbH, exaroton and it's associates and employees.</span>
+                </div>
+            </div>
+            <p>Welcome to Aternos Guides, an article site ran by Aternos regulars and an Aternos moderator. Here you may find all kinds of articles about topics and issues that are not covered in Aternos' help center itself. Have an issue that you don't know the fix to? Try our articles below, in the articles page or join the Aternos discord for help.</p>
+
+            <a href="about.php"><button class="btn btn-primary">About Us</button></a>
+            <a href="articles.php"><button class="btn btn-outline-primary">Articles</button></a>
+            <hr>
+        </div>
+
+        <div class="container pt-5">
+            <div class="float-start" style="margin-left: 25%">
+                <h2>Popular articles</h2>
+                <ul class="hidden">
+                    <li>Example article 1</li>
+                    <li>Example article 2</li>
+                    <li>Example article 3</li>
+                </ul>
+            </div>
+
+            <div class="float-end" style="margin-right: 25%">
+                <h2>Pinned articles</h2>
+                <ul class="hidden">
+                    <li>Example article 1</li>
+                    <li>Example article 2</li>
+                    <li>Example article 3</li>
+                </ul>
+            </div>
+        </div>
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     </body>
 </html>
