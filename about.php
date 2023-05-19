75290acaadf21a90a4092871fe6354c23297143e
Dobby-progam <87517487+Dobby-progam@users.noreply.github.com>
2023-05-19 17:31:06 +0200

    Rename About and Index to about and index for less trouble

--- about.php
+++ about.php
@@ -15,6 +15,16 @@ require_once('src/models/functions.php')
     <?php include_once 'src/models/navbar.php' ?>
 
     <body>
+    <div class="card" style="width: 18rem;">
+        <img src="" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">name</p>
+        </div>
+    </div>
+
+
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     </body>
 </html>
