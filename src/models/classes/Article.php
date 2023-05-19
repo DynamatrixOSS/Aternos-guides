76cefb613a814b1ed3a23b0c4c7e0468e5c37b02
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-19 16:38:30 +0200

    Remove obsolete code

--- src/models/classes/Article.php
+++ src/models/classes/Article.php
@@ -2,17 +2,6 @@
 
 class Article extends \Aternos\Model\GenericModel
 {
-    // use model registry (default: true)
-    protected static bool $registry = true;
-
-    // cache the model for 60 seconds (default: null)
-    protected static ?int $cache = 60;
-
-    // configure the generic model drivers (this is the default)
-    protected static array $drivers = [
-        \Aternos\Model\Driver\Mysqli\Mysqli::ID
-    ];
-
     // the name of your model (and table)
     public static function getName() : string
     {
