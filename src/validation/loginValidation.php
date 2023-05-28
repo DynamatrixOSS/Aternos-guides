a9296a9b81c6dfdf7a4081189072047e4ad7ae91
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-28 22:04:10 +0200

    Add .env path

--- src/validation/loginValidation.php
+++ src/validation/loginValidation.php
@@ -6,7 +6,7 @@ require_once '../../vendor/autoload.php';
 
 require_once('../models/functions.php');
 
-$dbCreds = databaseCredentials();
+$dbCreds = databaseCredentials('../../.env');
 
 $driver = new \Aternos\Model\Driver\Mysqli\Mysqli($dbCreds['host'], 3306, $dbCreds['user'], $dbCreds['password'], "", $dbCreds['database']);
 \Aternos\Model\Driver\DriverRegistry::getInstance()->registerDriver($driver);
