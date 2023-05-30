460ee9443f5265118df12702e0e687a30bd9c301
Dobby-progam <87517487+Dobby-progam@users.noreply.github.com>
2023-05-30 22:17:14 +0200

    css update on the register.php

--- register.php
+++ register.php
@@ -14,36 +14,56 @@
 
 <body>
 <form action="/src/validation/registrationValidation.php" method="post">
+    <div class="top row align-items-start">
+        <div class="col"><!--1/3 empty--!> </div>
+        <div class="col shadow">
+            <div class="row">
+                <div class="col top">
+                    <div class="">
+                        <img class="float-start img" src="src/img/Aternos_logo.svg" alt="Aternos Logo" >
+                    </div>
+                    <div class="">
+                        <img class="float-end img" src="src/img/Atomic.png" alt="Blue Atomic">
+                    </div>
+                    <div class="text ">
+                        <p class="text-warning-emphasis fs-5 ">If you are not part of the Blue Atomic team, this does not currently benefit you. We may implement features for registered accounts later.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="top">
+                <label for="username"><b>Username</b></label>
+                <input class="form-control" type="text" placeholder='Your preferred username' name="username" id="username" required>
+            </div>
+            <div class=" top">
+                <label for="mail"><b>Mail address</b></label>
+                <input class="form-control" type="email" placeholder="example@gmail.com" name="mail" id="mail" required>
+            </div>
+            <div class="top">
+                <label for="psw"><b>Password</b></label>
+                <input class="form-control" type="password" placeholder="Enter Password" name="psw" id="psw" required>
+            </div>
+            <?php
+            session_start();
 
-    <div class="container">
-        <p class="text-warning-emphasis w-50">If you are not part of the Blue Atomic team, this does not currently benefit you. We may implement features for registered accounts later.</p>
-
-        <label for="username"><b>Username</b></label>
-        <input type="text" placeholder='Your preferred username' name="username" id="username" required>
-
-        <label for="mail"><b>Mail address</b></label>
-        <input type="email" placeholder="example@gmail.com" name="mail" id="mail" required>
-
-        <label for="psw"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
-        <?php
-        session_start();
-
-        if (isset($_SESSION['exCode'])) {
-            if ($_SESSION['exCode'] == 404) {
-                echo "<p style='color: red;'>Unknown mail address.</p>";
-            } elseif ($_SESSION['exCode'] == 403) {
-                echo "<p style='color: red'>Wrong password</p>" ;
+            if (isset($_SESSION['exCode'])) {
+                if ($_SESSION['exCode'] == 404) {
+                    echo "<p style='color: red;'>Unknown mail address.</p>";
+                } elseif ($_SESSION['exCode'] == 403) {
+                    echo "<p style='color: red'>Wrong password</p>" ;
+                }
             }
-        }
-        session_destroy();
-        ?>
-        <button type="submit" name="login">Login</button>
-    </div>
+            session_destroy();
+            ?>
+            <div class="top">
+                <button class="btn btn-primary" type="submit" name="login">Login</button>
+                <button class="btn btn-primary" type="button" class="cancelbtn" onclick="history.back()">Cancel</button>
 
-    <div class="container footer">
-        <button type="button" class="cancelbtn" onclick="history.back()">Cancel</button>
-        <span class="psw"><a href="register.php">Already have an account?</a></span>
+            </div>
+            <div>
+                <span class="psw"><a href="register.php">Already have an account?</a></span>
+            </div>
+        </div>
+        <div class="col"><!--3/3 empty--!> </div>
     </div>
 </form>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
