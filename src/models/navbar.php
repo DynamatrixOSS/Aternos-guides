cb894a06f78c6ff0b6c0d196368d45409e29020e
Joshua Slui <joshuaslui0203@gmail.com>
2023-06-01 11:34:29 +0200

    Add articles href

--- src/models/navbar.php
+++ src/models/navbar.php
@@ -21,7 +21,7 @@
 
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                     <li><a href="../../index.php" class="nav-link px-2 text-white">Home</a></li>
-                    <li><a href="#" class="nav-link px-2 text-white">Articles</a></li>
+                    <li><a href="../../articles.php" class="nav-link px-2 text-white">Articles</a></li>
                     <li><a href="../../about.php" class="nav-link px-2 text-white">About</a></li>
                     <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
                 </ul>
