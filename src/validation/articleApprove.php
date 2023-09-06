492582f82844ffe45e6d39b1d25a398c0290d755
MiataBoy <joshua@intermixmanagement.nl>
2023-09-06 11:17:56 +0200

    add script for approving articles

---
+++ src/validation/articleApprove.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once ('../../vendor/autoload.php');
+
+require_once('../models/functions.php');
+
+$dbCreds = databaseCredentials('../../.env');
+
+$driver = new \Aternos\Model\Driver\Mysqli\Mysqli($dbCreds['host'], 3306, $dbCreds['user'], $dbCreds['password'], "", $dbCreds['database']);
+\Aternos\Model\Driver\DriverRegistry::getInstance()->registerDriver($driver);
+
+include "../models/classes/Article.php";
+
+$articleID = $_POST['article_id'];
+
+var_dump($articleID);
+
+$article = Article::select(["id"=>$articleID]);
+var_dump($article);
+$article[0]->approved = true;
+$article[0]->save();
+
+header('Location: ../../reviewing.php');
