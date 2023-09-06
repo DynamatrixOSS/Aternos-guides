f09e9f03e73fa3b203d8e25c9a6c7b16bae07d0a
MiataBoy <joshua@intermixmanagement.nl>
2023-09-06 08:12:17 +0200

    Add correct path to 'action' file

--- src/models/navbar.php
+++ src/models/navbar.php
@@ -26,7 +26,7 @@
                     <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
                 </ul>
 
-                <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search" action="search.php">
+                <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search" action="../validation/articleSearch.php">
                     <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search">
                 </form>
 
