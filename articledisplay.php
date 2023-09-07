4db3f5668d10c9864a09081d55446b3b4df50e3a
MiataBoy <joshuaslui0203@gmail.com>
2023-09-07 10:59:32 +0200

    Article name in page title

--- articledisplay.php
+++ articledisplay.php
@@ -11,13 +11,15 @@ $driver = new \Aternos\Model\Driver\Mysqli\Mysqli($dbCreds['host'], 3306, $dbCre
 \Aternos\Model\Driver\DriverRegistry::getInstance()->registerDriver($driver);
 
 include "src/models/classes/Article.php";
+
+$article_id = explode("-", explode("/", $_SERVER['REQUEST_URI'])[2]);
 ?>
 
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php  echo getPageName(basename($_SERVER['PHP_SELF'])) ?></title>
+    <title><?php  echo $article_id[1] ?></title>
     <script src="https://kit.fontawesome.com/d1393c407a.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="src/styling/main.css">
@@ -30,7 +32,6 @@ include "src/models/classes/Article.php";
 if (isset($_SESSION['authenticated'])) {
     $userQuery = User::select(["id" => $_SESSION['authenticated']]);
 }
-$article_id = explode("-", explode("/", $_SERVER['REQUEST_URI'])[2])[0];
 
 
 session_abort();
@@ -38,7 +39,7 @@ session_abort();
 <div class="pt-5 container">
     <div>
         <?php
-            $article = Article::select(["id" => $article_id]);
+            $article = Article::select(["id" => $article_id[0]]);
 
             if (count($article) === 0) {
                 echo '<h2>This article could not be found...</h2>';
@@ -46,7 +47,7 @@ session_abort();
                 if (isset($_SESSION['authenticated']) && ($userQuery[0]->roleID) >= 2) {
                     echo <<<EOL
         <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search" action="/src/validation/articleDelete.php" method="POST">
-            <input type="hidden" name="article_id" value="$article_id">
+            <input type="hidden" name="article_id" value="$article_id[0]">
             <button class="btn btn-danger" type="submit">Delete article</button>
         </form>
         EOL;
@@ -54,7 +55,7 @@ session_abort();
         if (isset($_SESSION['authenticated']) && ($userQuery[0]->roleID >= 1)) {
             echo <<<EOL
                     <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search" action="/editor.php" method="POST">
-            <input type="hidden" name="article_id" value="$article_id">
+            <input type="hidden" name="article_id" value="$article_id[0]">
             <button class="btn btn-warning" type="submit">Edit article</button>
         </form>
         <hr>
