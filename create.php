27172a9d8ae6a0fff5225060312db9fdf6f4075a
Dobby-progam <87517487+Dobby-progam@users.noreply.github.com>
2023-09-18 19:01:39 +0200

    made the content biger and added a link to MD

--- create.php
+++ create.php
@@ -40,6 +40,7 @@ session_abort();
         <div class="container">
         <h1>Article writer</h1>
             <form action="src/validation/articleCreate.php" method="post" class="left">
+            <p id="size" class="visually-hidden">what?</p>
                 <label for="title" class="h3">Title</label> <br>
                 <?php session_start(); if (isset($_SESSION['message'])) { echo $_SESSION['message'] . '<br>'; unset($_SESSION['message']);} ?>
                 <textarea name="title" id="title" placeholder="Why Aternos is amazing..." required></textarea> <br>
@@ -51,6 +52,7 @@ session_abort();
                 <textarea name="content" id="content" placeholder="Its amazing because..." required></textarea> <br>
                 <br>
                 <button class="btn btn-primary" type="submit">Submit</button>
+
             </form>
             <div class="right">
                 <h2>Remember:</h2>
@@ -59,6 +61,7 @@ session_abort();
                     <li>Use proper grammar</li>
                     <li>No other hosts</li>
                     <li>Only facts</li>
+                    <li>Use Markdown; <a href="https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax"> guide</a></li>
                 </ul>
             </div>
         </div>
