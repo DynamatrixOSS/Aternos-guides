f466d1e8f8321754f5b2cbf9d64502877340bbcd
Joshua Slui <joshuaslui0203@gmail.com>
2023-09-18 22:43:03 +0200

    Fix issue with css not loading

--- articledisplay.php
+++ articledisplay.php
@@ -23,12 +23,10 @@ $article = Article::select(["id" => $article_id[0]]);
     <title><?php echo 'Aternos Guides - ' . $article[0]->title ?></title>
     <script src="https://kit.fontawesome.com/d1393c407a.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="src/styling/main.css">
     <meta name="description" content="<?= $article[0]->summary ?>">
     <meta name="author" content="<?= $article[0]->title ?>">
+    <link rel="stylesheet" href="/src/styling/main.css">
 </head>
-
-<?php include_once 'src/models/navbar.php' ?>
 
 <body>
 <?php include_once 'src/models/navbar.php' ?>
@@ -45,21 +43,21 @@ session_abort();
     <div>
         <?php
 
-            if (count($article) === 0) {
-                echo '<h2>This article could not be found...</h2>';
-            } else {
-                $Parsedown = new Parsedown();
-                if (isset($_SESSION['authenticated']) && ($userQuery[0]->roleID) >= 2) {
-                    echo <<<EOL
+        if (count($article) === 0) {
+            echo '<h2>This article could not be found...</h2>';
+        } else {
+            $Parsedown = new Parsedown();
+            if (isset($_SESSION['authenticated']) && ($userQuery[0]->roleID) >= 2) {
+                echo <<<EOL
                         <div class="btn-group">            
                         <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search" action="/src/validation/articleDelete.php" method="POST">
                             <input type="hidden" name="article_id" value="$article_id[0]">
                             <button type="button" class="btn btn-danger" type="submit">Delete article</button>
                         </form>
                         EOL;
-                }
-                if (isset($_SESSION['authenticated']) && ($userQuery[0]->roleID >= 1)) {
-                    echo <<<EOL
+            }
+            if (isset($_SESSION['authenticated']) && ($userQuery[0]->roleID >= 1)) {
+                echo <<<EOL
                         <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search" action="/editor.php" method="POST">
                             <input type="hidden" name="article_id" value="$article_id[0]">
                             <button class="btn btn-warning" type="submit">Edit article</button>
@@ -70,10 +68,10 @@ session_abort();
                         <h2>{$article[0]->title}</h2>
                         {$Parsedown->parse($article[0]->content)}
                     EOL;
-                }
-                $article[0]->views++;
-                $article[0]->save();
             }
+            $article[0]->views++;
+            $article[0]->save();
+        }
         ?>
     </div>
 </div>
