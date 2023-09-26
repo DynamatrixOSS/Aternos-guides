ad97815b8cf30a30f6e1f2a28a38f1f966ae1537
MiataBoy <joshua@intermixmanagement.nl>
2023-09-26 16:54:43 +0200

    Add user role edit handler

---
+++ src/validation/userEditRole.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once ('../../vendor/autoload.php');
+
+require_once('../models/functions.php');
+
+$dbCreds = databaseCredentials('../../.env');
+
+$driver = new \Aternos\Model\Driver\Mysqli\Mysqli($dbCreds['host'], 3306, $dbCreds['user'], $dbCreds['password'], "", $dbCreds['database']);
+\Aternos\Model\Driver\DriverRegistry::getInstance()->registerDriver($driver);
+
+include "../models/classes/User.php";
+
+$user = User::get($_POST['user_id']);
+
+$user->roleID = $_POST['role'];
+$user->save();
+
+header("Location: /user/$user->username");
+exit();
