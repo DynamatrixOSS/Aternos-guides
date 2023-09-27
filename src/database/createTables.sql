eda2a0cbd34216fc4714fd0e27aec3c6aa85e7de
MiataBoy <joshua@intermixmanagement.nl>
2023-09-27 16:01:30 +0200

    add about to users

--- src/database/createTables.sql
+++ src/database/createTables.sql
@@ -3,13 +3,13 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(255) UNIQUE,
     mail NVARCHAR(256) UNIQUE,
     password VARCHAR(255),
-    roleID INT(1)
     roleID INT(1),
     about VARCHAR(1000) DEFAULT 'I use Aternos Guides!'
 );
 
 CREATE TABLE IF NOT EXISTS articles (
     id VARCHAR(16) PRIMARY KEY,
+    author VARCHAR(255),
     title VARCHAR(260) unique,
     summary VARCHAR(260),
     content LONGTEXT,
