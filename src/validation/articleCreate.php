1df5a036dabc123e0213d984affdf81ce66e8878
MiataBoy <joshua@intermixmanagement.nl>
2023-09-27 15:59:40 +0200

    Add author to articles

--- src/validation/articleCreate.php
+++ src/validation/articleCreate.php
@@ -10,20 +10,25 @@ $driver = new \Aternos\Model\Driver\Mysqli\Mysqli($dbCreds['host'], 3306, $dbCre
 \Aternos\Model\Driver\DriverRegistry::getInstance()->registerDriver($driver);
 
 include "../models/classes/Article.php";
+include "../models/classes/User.php";
+
 
 
 $article = new Article();
 $article->title = $_POST['title'];
 
+session_start();
+
 $ArticleExists = Article::select(["title"=>$_POST['title']]);
+$authorName = User::get($_SESSION['authenticated'])->username;
 
 if ($ArticleExists->wasSuccessful() && count($ArticleExists) !== 0) {
-    session_start();
     var_dump($ArticleExists);
     $_SESSION['message'] = "An article with the title " . $ArticleExists[0]->title . " already exists";
     Header('Location: ../../../create');
 }
 
+$article->author = $authorName;
 $article->summary = $_POST['summary'];
 $article->content = $_POST['content'];
 $article->views = 0;
