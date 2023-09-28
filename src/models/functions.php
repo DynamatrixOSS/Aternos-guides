611525a7b79f5f5fc12417ac6e4bb93e0efd1f1c
MiataBoy <joshua@intermixmanagement.nl>
2023-09-28 11:57:34 +0200

    Use new functions

--- src/models/functions.php
+++ src/models/functions.php
@@ -1,4 +1,8 @@
 <?php
+
+use Aternos\Model\models\classes\User;
+use Aternos\Model\models\classes\Article;
+
 function getPageName(string $page): string {
     $name = explode(".", $page)[0];
     if ($name === 'index') {
@@ -17,4 +21,40 @@ function databaseCredentials(string $pathToEnv): array {
     $database = $env['DATABASE'];
 
     return ["host" => $host, "user" => $user, "password" => $password, "database" => $database];
+}
+
+function getAuthUser(): User|Array {
+    session_start();
+
+    if (!isset($_SESSION['authenticated'])) {
+        return ["status" => 401, "data" => "You must be logged in to access this."];
+    }
+
+    return User::get($_SESSION['authenticated']);
+}
+
+function selectUser(string $key, string|int $value): User|Array {
+    session_start();
+
+    $user = User::select([$key=>$value])[0];
+
+    session_abort();
+    if (!$user) {
+        return ["status" => 404, "data" => "A user with this $key could not be found."];
+    }
+
+    return ["status" => 200, "data" => $user];
+}
+
+function selectArticle(string $key, string|int $value): User|Array {
+    session_start();
+
+    $article = Article::select([$key=>$value])[0];
+
+    session_abort();
+    if (!$article) {
+        return ["status" => 404, "data" => "An article with this $key could not be found."];
+    }
+
+    return ["status" => 200, "data" => $article];
 }
